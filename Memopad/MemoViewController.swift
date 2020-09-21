f440f5ab26737e5850ebd785b2ec2c07061f62fc
Souta Kawamura <soutakawamura@icloud.com>
2020-09-21 20:48:47 +0900

    first commit

---
+++ Memopad/MemoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoViewController.swift
+//  Memopad
+//
+//  Created by チップまん on 2020/09/19.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
