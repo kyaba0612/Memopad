bce5b7bea8c7bb5a99dcb9e6ce5bd4679f52b539
kyaba0612 <soutakawamura@icloud.com>
2020-09-21 21:57:41 +0900

    first commit

--- Memopad/MemoViewController.swift
+++ Memopad/MemoViewController.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
-class MemoViewController: UIViewController {
+class MemoViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var titleTextField:UITextField!
+    @IBOutlet var contentTextView: UITextView!
+    
+    var saveData: UserDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        titleTextField.text = saveData.object(forKey: "title") as? String
+        contentTextView.text = saveData.object(forKey: "content") as? String
+        titleTextField.delegate = self
     }
     
-
+    @IBAction func saveMemo(){
+        
+        saveData.set(titleTextField.text, forKey: "title")
+        saveData.set(contentTextView.text, forKey: "content")
+        
+        let alert: UIAlertController = UIAlertController(title:"保存",message:"メモの保存が完了しました。",preferredStyle: .alert)
+        
+        alert.addAction(
+                UIAlertAction(
+                    title: "OK",
+                    style: .default,
+                    handler: { action in
+                        
+                        self.navigationController?.popViewController(animated: true)
+                    }
+                )
+            )
+        present(alert,animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     /*
     // MARK: - Navigation
 
---
+++ Memopad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memopad
+//
+//  Created by チップまん on 2020/09/19.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
